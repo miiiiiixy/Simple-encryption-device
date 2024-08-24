94ab85b741bfe2b18b7752376df7f397e85e198c
miiiiiixy <2206284157@qq.com>
2024-08-24 21:04:49 +0800

    Add files via upload

--- 简易加密器.cpp
+++ 简易加密器.cpp
@@ -19,16 +19,17 @@ int main()
 		
 		
 		//将所有的字母在字母表的顺序上向左移动3格 
-		if(a[i]>='a'&&a[i]<='c'||a[i]>='A'&&a[i]<='C') a[i]+=23;//Determine if it is in the first three letters, if so, special treatment
-		else if(a[i]>='d'&&a[i]<='z'||a[i]>='D'&&a[i]<='Z') a[i]-=3;//Otherwise, handle it normally
+		if(a[i]>='a'&&a[i]<='c'||a[i]>='A'&&a[i]<='C') a[i]+=23;//如果是前三个字母，特殊处理 
+		else if(a[i]>='d'&&a[i]<='z'||a[i]>='D'&&a[i]<='Z') a[i]-=3;//否则正常处理 
 		//标点符号不处理 
 	}
-	cout<<"加密后的文本是："<<endl;
+	cout<<"加密后的文本是："<<endl<<endl;
 	for(int i=a.size()-1;i>-1;i--)
 	{
 		//反转并输出加密后的文本 
 		cout<<a[i];
 	}
-	cin>>b;
+	cout<<endl<<endl<<"解密完毕，关闭窗口即可结束程序:)"; //多用几个回车美观一些 
+	cin>>b;//用输入保证程序不会被关闭，如果谁有更好的办法教教我吧，我的c++技术太差了:( 
 	return 0;
 }
